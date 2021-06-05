4b6f228c996d6e277a197241546a5ff38cc080aa
ananya8606 <52853286+ananya8606@users.noreply.github.com>
2021-06-05 10:45:51 +0530

    Create Techstack.js

--- src/components/Techstack.js
+++ src/components/Techstack.js
@@ -16,14 +16,32 @@ function Techstack() {
   return (
       <p className="tech-icons">
         <CgCPlusPlus />
+</p>
+ <p className="tech-icons">
         <DiJavascript1 />
+</p>
+ <p className="tech-icons">
         <DiNodejs />
+</p>
+ <p className="tech-icons">
         <DiReact />
+</p>
+ <p className="tech-icons">
         <DiMongodb />
+</p>
+ <p className="tech-icons">
         <DiGit />
+</p>
+ <p className="tech-icons">
         <SiFirebase />
+</p>
+ <p className="tech-icons">
         <DiBootstrap />
+</p>
+ <p className="tech-icons">
         <DiHtml5 />
+</p>
+ <p className="tech-icons">
         <DiCss3 />
       </p>
   );
