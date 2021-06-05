6a9172aabd2bf58b0cb00d7f5328bbc85fbb7b27
ananya8606 <52853286+ananya8606@users.noreply.github.com>
2021-06-05 09:59:14 +0530

    Create Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,7 +7,7 @@ function Projects() {
         <h1 className="project-heading">
           My Recent <strong className="purple">Works </strong>
         </h1>
-        <p style={{ color:"#f25ca2"}}>
+        <p style={{ color:"#f25ca2","font-size:25px"}}>
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
